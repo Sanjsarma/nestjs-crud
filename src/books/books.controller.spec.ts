82622027658e71570faf956191d67bbf688afb7a
Sanjana <sangeethasanjana@gmail.com>
2023-06-04 14:19:11 +0530

    Basic controller, service unit tests added

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,13 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { mock } from 'node:test';
 import { BooksController } from './books.controller';
+import { BooksService } from './books.service';
 
 describe('BooksController', () => {
   let controller: BooksController;
 
+  const mockBooksService = {
+    create: jest.fn(dto => { //mock create function in service
+      return {
+      id: Math.random(),
+      ...dto
+      }
+    }),
+    findAll: jest.fn(dto => {
+      return {
+        ...dto
+      }
+    }),
+    update: jest.fn((id, dto) => {
+      return {
+        id,
+        ...dto
+      }
+    })
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BooksController],
-    }).compile();
+      providers: [BooksService]
+    }).overrideProvider(BooksService).useValue(mockBooksService).compile();
 
     controller = module.get<BooksController>(BooksController);
   });
@@ -15,4 +37,28 @@ describe('BooksController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a book', () => {
+    let createBookDto = {name: 'book', author: 'sanj', isPresent: true};
+    expect(controller.createBook(createBookDto)).toEqual({
+      id: expect.any(Number),
+      name: createBookDto.name, 
+      author: createBookDto.author, 
+      isPresent: createBookDto.isPresent
+    })
+    expect(mockBooksService.create).toHaveBeenCalled();
+    expect(mockBooksService.create).toHaveBeenCalledWith(createBookDto);
+  })
+
+  it('should update book by id', () => {
+    expect(mockBooksService.update).toBeDefined();
+    let updateBookDto = {name: 'book', author: 'sanj', isPresent: false };
+    let id = 1;
+    expect(controller.updateBook(id, updateBookDto)).toEqual({
+      id: 1,
+      ...updateBookDto
+    })
+    expect(mockBooksService.update).toHaveBeenCalled();
+
+  })
 });
--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Book } from './book.entity';
 import { BooksService } from './books.service';
 
 describe('BooksService', () => {
   let service: BooksService;
 
+  const mockBooksRepository = {
+    create: jest.fn(dto => dto),
+    save: jest.fn(dto => {
+      return Promise.resolve({id: Math.random(), ...dto})
+    })
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BooksService],
+      providers: [BooksService, 
+      {    
+        provide: getRepositoryToken(Book), //for mocking repositories
+        useValue: mockBooksRepository
+      },
+    ]
     }).compile();
 
     service = module.get<BooksService>(BooksService);
@@ -15,4 +29,14 @@ describe('BooksService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should create a new book', async() => {
+    let updateBookDto = {name: 'book', author: 'sanj', isPresent: true};
+    expect(await service.create(updateBookDto)).toEqual({
+      id: expect.any(Number),
+      name: updateBookDto.name,
+      author: updateBookDto.author,
+      isPresent: updateBookDto.isPresent
+    })
+  } )
 });
